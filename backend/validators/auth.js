705a0d3bb05d459696ae54b6d63f406abfb8a9fb
Alexey_Lisenko <detoner777@gmail.com>
2019-09-30 19:51:56 +0300

    auth routes and auth validation

---
+++ backend/validators/auth.js
@@ -0,0 +1,13 @@
+const { check } = require("express-validator");
+exports.userSignupValidator = [
+  check("name")
+    .not()
+    .isEmpty()
+    .withMessage("Name is required"),
+  check("email")
+    .isEmail()
+    .withMessage("Musrt bea valid email address"),
+  check("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long")
+];
