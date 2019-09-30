0fa93bf4961d1ad76a6c527c61738ebb735f3787
Alexey_Lisenko <detoner777@gmail.com>
2019-09-30 18:57:36 +0300

    initial routes

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,10 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
+//bring routes
+const blogRoutes = require("./routes/blog");
 
-//ap
+//app
 const app = express();
 
 //db
@@ -14,7 +16,8 @@ mongoose
   .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true
   })
   .then(() => console.log("DB connected"))
   .catch(err => {
@@ -29,11 +32,9 @@ app.use(cookieParser());
 if (process.env.NODE_ENV === "development") {
   app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
 }
+//routes middleware
+app.use("/api", blogRoutes);
 
-//routes
-app.get("/api", (req, res) => {
-  res.json({ time: Date().toString() });
-});
 //port
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
