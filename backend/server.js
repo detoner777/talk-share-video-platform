a9f4ea1bd87ecaa1f0d9409d15e19e9df155dd94
Alexey_Lisenko <detoner777@gmail.com>
2019-09-30 15:30:29 +0300

    initial MongoDB

--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,32 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-require("dotenv").config;
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 //ap
 const app = express();
+
+//db
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log("DB connected"))
+  .catch(err => {
+    console.log(err);
+  });
 
 //middlewars
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 //cors
-app.use(cors());
+if (process.env.NODE_ENV === "development") {
+  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+}
 
 //routes
 app.get("/api", (req, res) => {
