8c346ee3bd5d050b3617aa00953a9c6894d936a3
Alexey_Lisenko <detoner777@gmail.com>
2019-10-02 18:25:55 +0300

    SigninComponent.js

--- frontend/pages/signup.js
+++ frontend/pages/signup.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 const Signup = ({ children }) => {
   return (
     <Layout>
-      <h2 className="text-center pt-4 pb-4">Signup page</h2>
+      <h2 className="text-center pt-4 pb-4">Signup</h2>
       <div className="row">
         <div className="col-md-6 offset-md-3">
           <SignupComponent />
