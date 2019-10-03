174dd9e9e4fa4c5024945d90e4d49fc1aea4d8c9
Alexey_Lisenko <detoner777@gmail.com>
2019-10-03 12:34:26 +0300

    Signout and conditionally showing nav links

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import Link from "next/link";
+import Router from "next/router";
 import { APP_NAME } from "../config";
+import { signout, isAuth } from "../actions/auth";
+
 import {
   Collapse,
   Navbar,
@@ -31,16 +34,31 @@ const Header = () => {
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="ml-auto" navbar>
-            <NavItem>
-              <Link href="/signin">
-                <NavLink>Signin</NavLink>
-              </Link>
-            </NavItem>
-            <NavItem>
-              <Link href="/signup">
-                <NavLink>Signup</NavLink>
-              </Link>
-            </NavItem>
+            {!isAuth() && (
+              <React.Fragment>
+                {" "}
+                <NavItem>
+                  <Link href="/signin">
+                    <NavLink style={{ cursor: "pointer" }}>Signin</NavLink>
+                  </Link>
+                </NavItem>
+                <NavItem>
+                  <Link href="/signup">
+                    <NavLink style={{ cursor: "pointer" }}>Signup</NavLink>
+                  </Link>
+                </NavItem>
+              </React.Fragment>
+            )}
+            {isAuth() && (
+              <NavItem>
+                <NavLink
+                  style={{ cursor: "pointer" }}
+                  onClick={() => signout(() => Router.replace(`/signin`))}
+                >
+                  Signout
+                </NavLink>
+              </NavItem>
+            )}
           </Nav>
         </Collapse>
       </Navbar>
