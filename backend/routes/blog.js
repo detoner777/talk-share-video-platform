5e705e8a5bba1ffe82fe3238b3bbe1d410ca2d61
Alexey_Lisenko <detoner777@gmail.com>
2019-10-12 11:37:29 +0300

    bug fixed on CreateBlog

--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { create } = require("../controllers/blog");
+
 const { requireSignin, adminMiddleware } = require("../controllers/auth");
 
 router.post("/blog", requireSignin, adminMiddleware, create);
