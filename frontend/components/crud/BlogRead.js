55b731733f8a751e33ca32daf2afb89049c78a3b
Alexey_Lisenko <detoner777@gmail.com>
2019-10-19 21:08:53 +0300

    List of blogs fotr admin

---
+++ frontend/components/crud/BlogRead.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import Router from "next/router";
+
+import { getCookie, isAuth } from "../../actions/auth";
+
+import { list, removeBlog } from "../../actions/blog";
+
+const BlogRead = () => {
+  return (
+    <React.Fragment>
+      <p>Update delete blogs</p>
+    </React.Fragment>
+  );
+};
+
+export default BlogRead;
