b63e369e779cf88d0dfecce3cc6100d2b43e0dec
Alexey_Lisenko <detoner777@gmail.com>
2019-10-27 11:52:09 +0200

    Tags page SEO header

--- backend/controllers/tag.js
+++ backend/controllers/tag.js
@@ -1,4 +1,5 @@
 const Tag = require("../models/tag");
+const Blog = require("../models/blog");
 const slugify = require("slugify");
 const { errorHandler } = require("../helpers/dbErrorHandler");
 
@@ -10,11 +11,12 @@ exports.create = (req, res) => {
 
   tag.save((err, data) => {
     if (err) {
+      console.log(err);
       return res.status(400).json({
         error: errorHandler(err)
       });
     }
-    res.json(data);
+    res.json(data); // dont do this res.json({ tag: data });
   });
 };
 
@@ -35,10 +37,25 @@ exports.read = (req, res) => {
   Tag.findOne({ slug }).exec((err, tag) => {
     if (err) {
       return res.status(400).json({
-        error: errorHandler(err)
+        error: "Tag not found"
       });
     }
-    res.json(tag);
+    // res.json(tag);
+    Blog.find({ tags: tag })
+      .populate("categories", "_id name slug")
+      .populate("tags", "_id name slug")
+      .populate("postedBy", "_id name")
+      .select(
+        "_id title slug excerpt categories postedBy tags createdAt updatedAt"
+      )
+      .exec((err, data) => {
+        if (err) {
+          return res.status(400).json({
+            error: errorHandler(err)
+          });
+        }
+        res.json({ tag: tag, blogs: data });
+      });
   });
 };
 
