13265a281a3a82b3eedcc4c712ac384686b41ce4
Alexey_Lisenko <detoner777@gmail.com>
2019-10-30 19:50:09 +0200

    Profile update and show

--- frontend/components/auth/ProfileUpdate.js
+++ frontend/components/auth/ProfileUpdate.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 import Router from "next/router";
 import { getCookie, isAuth } from "../../actions/auth";
 import { getProfile, update } from "../../actions/user";
-import { loadGetInitialProps } from "next/dist/next-server/lib/utils";
+import { API } from "../../config";
 
 const ProfileUpdate = () => {
   const [values, setValues] = useState({
@@ -24,19 +24,19 @@ const ProfileUpdate = () => {
     username,
     name,
     email,
+    about,
     password,
     error,
     success,
     loading,
     photo,
-    about,
     userData
   } = values;
 
   const init = () => {
     getProfile(token).then(data => {
       if (data.error) {
-        setValues({ ...values, erro: data.error });
+        setValues({ ...values, error: data.error });
       } else {
         setValues({
           ...values,
@@ -54,11 +54,12 @@ const ProfileUpdate = () => {
   }, []);
 
   const handleChange = name => e => {
+    // console.log(e.target.value);
     const value = name === "photo" ? e.target.files[0] : e.target.value;
     let userFormData = new FormData();
     userFormData.set(name, value);
     setValues({
-      ...value,
+      ...values,
       [name]: value,
       userData: userFormData,
       error: false,
@@ -84,6 +85,7 @@ const ProfileUpdate = () => {
           name: data.name,
           email: data.email,
           about: data.about,
+          password: "",
           success: true,
           loading: false
         });
@@ -91,78 +93,117 @@ const ProfileUpdate = () => {
     });
   };
 
-  const profileUpdateForm = () => {
-    return (
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label className="btn btn-outline-info">Profile Photo</label>
+  const profileUpdateForm = () => (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label className="btn btn-outline-info">
+          Profile photo
           <input
             onChange={handleChange("photo")}
             type="file"
             accept="image/*"
             hidden
           />
-        </div>
-        <div className="form-group">
-          <label className="text-muted">Username</label>
-          <input
-            onChange={handleChange("username")}
-            type="text"
-            value={username}
-            className="form-control"
-          />
-        </div>
-        <div className="form-group">
-          <label className="text-muted">Name</label>
-          <input
-            onChange={handleChange("name")}
-            type="text"
-            value={name}
-            className="form-control"
-          />
-        </div>
-        <div className="form-group">
-          <label className="text-muted">Email</label>
-          <input
-            onChange={handleChange("email")}
-            type="text"
-            value={email}
-            className="form-control"
-          />
-        </div>
-        <div className="form-group">
-          <label className="text-muted">About</label>
-          <textarea
-            onChange={handleChange("about")}
-            type="text"
-            value={about}
-            className="form-control"
-          />
-        </div>
-        <div className="form-group">
-          <label className="text-muted">Password</label>
-          <input
-            onChange={handleChange("password")}
-            type="text"
-            value={password}
-            className="form-control"
-          />
-        </div>
-        <div>
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </div>
-      </form>
-    );
-  };
+        </label>
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Username</label>
+        <input
+          onChange={handleChange("username")}
+          type="text"
+          value={username}
+          className="form-control"
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Name</label>
+        <input
+          onChange={handleChange("name")}
+          type="text"
+          value={name}
+          className="form-control"
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Email</label>
+        <input
+          onChange={handleChange("email")}
+          type="text"
+          value={email}
+          className="form-control"
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">About</label>
+        <textarea
+          onChange={handleChange("about")}
+          type="text"
+          value={about}
+          className="form-control"
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Password</label>
+        <input
+          onChange={handleChange("password")}
+          type="password"
+          value={password}
+          className="form-control"
+        />
+      </div>
+      <div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+
+  const showError = () => (
+    <div
+      className="alert alert-danger"
+      style={{ display: error ? "" : "none" }}
+    >
+      {error}
+    </div>
+  );
+
+  const showSuccess = () => (
+    <div
+      className="alert alert-success"
+      style={{ display: success ? "" : "none" }}
+    >
+      Profile updated
+    </div>
+  );
+
+  const showLoading = () => (
+    <div
+      className="alert alert-info"
+      style={{ display: loading ? "" : "none" }}
+    >
+      Loading...
+    </div>
+  );
 
   return (
     <React.Fragment>
       <div className="container">
         <div className="row">
-          <div className="col-md-4">image</div>
-          <div classNam="col-md-8">{profileUpdateForm()}</div>
+          <div className="col-md-4">
+            <img
+              src={`${API}/user/photo/${username}`}
+              className="img img-fluid img-thumbnail mb-3"
+              style={{ maxHeight: "auto", maxWidth: "100%" }}
+              alt="user profile"
+            />
+          </div>
+          <div className="col-md-8 mb-5">
+            {showSuccess()}
+            {showError()}
+            {showLoading()}
+            {profileUpdateForm()}
+          </div>
         </div>
       </div>
     </React.Fragment>
