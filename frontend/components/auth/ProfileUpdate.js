92050d1c5ae75c9abf9ed752660b434cf762a674
Alexey_Lisenko <detoner777@gmail.com>
2019-10-30 16:57:59 +0200

    ProfileUpdate component in develop

--- frontend/components/auth/ProfileUpdate.js
+++ frontend/components/auth/ProfileUpdate.js
@@ -1,2 +1,68 @@
-const ProfileUpdate = () => <h2>profile update</h2>;
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import Router from "next/router";
+import { getCookie, isAuth } from "../../actions/auth";
+import { getProfile, update } from "../../actions/user";
+import { loadGetInitialProps } from "next/dist/next-server/lib/utils";
+
+const ProfileUpdate = () => {
+  const [values, setValues] = useState({
+    username: "",
+    name: "",
+    email: "",
+    password: "",
+    error: false,
+    success: false,
+    loading: false,
+    photo: "",
+    userData: ""
+  });
+
+  const token = getCookie("token");
+  const {
+    username,
+    name,
+    email,
+    password,
+    error,
+    success,
+    loading,
+    photo,
+    userData
+  } = values;
+
+  const init = () => {
+    getProfile(token).then(data => {
+      if (data.error) {
+        setValues({ ...values, erro: data.error });
+      } else {
+        setValues({
+          ...values,
+          username: data.username,
+          name: data.name,
+          email: data.email,
+          about: data.about
+        });
+      }
+    });
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  return (
+    <React.Fragment>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">image</div>
+          <div classNam="col-md-8">
+            update form
+            {JSON.stringify({ username, email, name })}
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
 export default ProfileUpdate;
