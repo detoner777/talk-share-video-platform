cda3a1cd515a31c99d5465b611fcf39fbc6e25c7
Alexey_Lisenko <detoner777@gmail.com>
2019-11-26 07:31:38 +0200

    Change index page

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -14,7 +14,7 @@ const Index = () => {
             </div>
           </div>
         </div>
-
+        {/* -> */}
         <div className="container">
           <div className="row">
             <div className="col-md-12 text-center pt-4 pb-5">
@@ -33,23 +33,23 @@ const Index = () => {
                   style={{
                     backgroundImage:
                       "url(" +
-                      "https://images.pexels.com/photos/540518/pexels-photo-540518.jpeg" +
+                      "https://nofilmschool.com/sites/default/files/styles/facebook/public/uploads/2014/04/cinema-lens.jpg?itok=dWQgUKep" +
                       ")"
                   }}
                 >
-                  <h2 className="text-shadow text-center h1">Videos</h2>
+                  <h2 className="text-shadow text-center h1">Video shooting</h2>
                 </div>
                 <div className="back text-center">
-                  <Link href="/categories/videos">
+                  <Link href="/categories/shooting">
                     <a>
-                      <h3 className="h1">React Js</h3>
+                      <h3 className="h1">Video tricks:</h3>
                     </a>
                   </Link>
                   <p className="lead">A bla-bla-bla text</p>
                 </div>
               </div>
             </div>
-
+            {/* -> */}
             <div className="col-md-4">
               <div className="flip flip-horizontal">
                 <div
@@ -57,11 +57,63 @@ const Index = () => {
                   style={{
                     backgroundImage:
                       "url(" +
-                      "https://images.pexels.com/photos/540518/pexels-photo-540518.jpeg" +
+                      "https://blog.pond5.com/wp-content/uploads/2016/08/Screenwriting_Narrative_Arc.jpg" +
                       ")"
                   }}
                 >
-                  <h2 className="text-shadow text-center h1">Locations</h2>
+                  <h2 className="text-shadow text-center h1">
+                    Scripting ideas
+                  </h2>
+                </div>
+                <div className="back text-center">
+                  <Link href="/categories/scripting">
+                    <a>
+                      <h3 className="h1">Script notes</h3>
+                    </a>
+                  </Link>
+                  <p className="lead">A bla-bla-bla text</p>
+                </div>
+              </div>
+            </div>
+            {/* -> */}
+            <div className="col-md-4">
+              <div className="flip flip-horizontal">
+                <div
+                  className="front"
+                  style={{
+                    backgroundImage:
+                      "url(" +
+                      "http://keyassets.timeincuk.net/inspirewp/live/wp-content/uploads/sites/33/2017/10/photo-1462926795244-b273f8a5454f-e1507212311767-630x361.jpg" +
+                      ")"
+                  }}
+                >
+                  <h2 className="text-shadow text-center h1">Gears</h2>
+                </div>
+                <div className="back text-center">
+                  <Link href="/categories/gears">
+                    <a>
+                      <h3 className="h1">Gears</h3>
+                    </a>
+                  </Link>
+                  <p className="lead">A bla-bla-bla text</p>
+                </div>
+              </div>
+            </div>
+            {/* -> */}
+            <div className="col-md-4">
+              <div className="flip flip-horizontal">
+                <div
+                  className="front"
+                  style={{
+                    backgroundImage:
+                      "url(" +
+                      "https://images.unian.net/photos/2017_04/1491483849-9026-reproduktsiya-podolsko-voskresenskogo-mosta-foto-unian.jpg" +
+                      ")"
+                  }}
+                >
+                  <h2 className="text-shadow text-center h1">
+                    Locations in Kiev
+                  </h2>
                 </div>
                 <div className="back text-center">
                   <Link href="/categories/locations">
@@ -73,7 +125,9 @@ const Index = () => {
                 </div>
               </div>
             </div>
+            {/* -> */}
 
+            {/* -> */}
             <div className="col-md-4">
               <div className="flip flip-horizontal">
                 <div
@@ -81,24 +135,50 @@ const Index = () => {
                   style={{
                     backgroundImage:
                       "url(" +
-                      "https://images.pexels.com/photos/540518/pexels-photo-540518.jpeg" +
+                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpcfwSJf8sQbLLACLkOdAR7ygXuRCm39qyGQoXybuq-XRJqDai&s" +
                       ")"
                   }}
                 >
                   <h2 className="text-shadow text-center h1">
-                    Content creations
+                    POST Production
                   </h2>
                 </div>
                 <div className="back text-center">
-                  <Link href="/categories/Content creations">
+                  <Link href="/categories/post">
                     <a>
-                      <h3 className="h1">Content creations</h3>
+                      <h3 className="h1">Post</h3>
                     </a>
                   </Link>
                   <p className="lead">A bla-bla-bla text</p>
                 </div>
               </div>
             </div>
+            {/* -> */}
+            {/* -> */}
+            <div className="col-md-4">
+              <div className="flip flip-horizontal">
+                <div
+                  className="front"
+                  style={{
+                    backgroundImage:
+                      "url(" +
+                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxZNPtT3vOUkHFqXbHADba4KrQc7XUfXS2dJ4SucDjljS8tHBf&s" +
+                      ")"
+                  }}
+                >
+                  <h2 className="text-shadow text-center h1">Industry News</h2>
+                </div>
+                <div className="back text-center">
+                  <Link href="/categories/news">
+                    <a>
+                      <h3 className="h1">News</h3>
+                    </a>
+                  </Link>
+                  <p className="lead">A bla-bla-bla text</p>
+                </div>
+              </div>
+            </div>
+            {/* -> */}
           </div>
         </div>
       </article>
